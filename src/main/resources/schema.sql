ac20e9ebb99d88ce193ec96080339caa26a52d72
artemiss <divinehemlock@gmail.com>
2023-05-21 17:47:31 +0330

    schema created

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS public.student;
+CREATE TABLE IF NOT EXISTS public.student(
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS public.course;
+CREATE TABLE IF NOT EXISTS public.course(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    professor VARCHAR(255),
+    semester VARCHAR(255)
+);
+
+DROP TABLE IF EXISTS public.student_course;
+CREATE TABLE IF NOT EXISTS public.student_course(
+    id SERIAL PRIMARY KEY,
+    student_id int,
+    course_id int
+);
